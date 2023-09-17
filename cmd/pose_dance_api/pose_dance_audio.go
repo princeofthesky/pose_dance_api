dccb7adbf5089fa02ef2cc426806434b3967411e
阿心-Tam Alex <alextam2791@gmail.com>
2023-09-17 18:06:27 +0800

    update

--- cmd/pose_dance_api/pose_dance_audio.go
+++ cmd/pose_dance_api/pose_dance_audio.go
@@ -8,11 +8,13 @@ import (
 	"go-pinterest/model"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 var MapSongs = map[string]model.SongDetailInfo{}
 var ListSong []model.SongDetailInfo
+var length = 20
 
 func InitListAudio() {
 	defer func() {
@@ -58,6 +60,34 @@ func InitListAudio() {
 		}
 		frames := data["frames"]
 		songDetail := model.SongDetailInfo{Info: songInfo, Frames: frames}
+		var err error
+		songDetail.Info.TotalMatch, err = db.GetTotalMatchResultByAudioId(songDetail.Info.AudioId)
+		if err != nil {
+			println("err when get total match by audio id ", songDetail.Info.AudioId, " err ", err.Error())
+		}
+		songDetail.Info.TotalPlayer, err = db.GetTotalPlayerByAudioId(songDetail.Info.AudioId)
+		if err != nil {
+			println("err when get total player by audio id ", songDetail.Info.AudioId, " err ", err.Error())
+		}
+		bestMatches, err := db.GetBestMatchResultByAudioId(songDetail.Info.AudioId, 1)
+		if err != nil {
+			println("err when get best result by audio id ", songDetail.Info.AudioId, " err ", err.Error())
+		}
+		if len(bestMatches) > 0 && bestMatches[0].Id > 0 {
+			user, err := db.GetUserById(bestMatches[0].UserId)
+			if err != nil {
+				println("err when get user info by id ", bestMatches[0].UserId, " err ", err.Error())
+			}
+			songDetail.Info.BestResult = model.ScoreResponse{
+				Youtube: MapYoutubeVideos[bestMatches[0].Id],
+				Match:   &bestMatches[0],
+				User: &model.BasicUserInfo{
+					Id:     user.Id,
+					Name:   user.Name,
+					Avatar: user.Avatar,
+				},
+			}
+		}
 		NewListSongs = append(NewListSongs, songDetail)
 		NewMapSongs[songDetail.Info.AudioId] = songDetail
 	}
@@ -69,35 +99,18 @@ func InitListAudio() {
 
 func GetListAudios(c *gin.Context) {
 	var allSongs []model.SongBasicInfo
-	for _, song := range ListSong {
-		var err error
-		song.Info.TotalMatch, err = db.GetTotalMatchResultByAudioId(song.Info.AudioId)
-		if err != nil {
-			println("err when get total match by audio id ", song.Info.AudioId, " err ", err.Error())
+	pageText, exit := c.GetQuery("page")
+	if !exit {
+		pageText = "0"
+	}
+	page, _ := strconv.Atoi(pageText)
+	start := page * length
+	end := start + length
+	for i := start; i < end; i++ {
+		if i >= len(ListSong) {
+			break
 		}
-		song.Info.TotalPlayer, err = db.GetTotalPlayerByAudioId(song.Info.AudioId)
-		if err != nil {
-			println("err when get total player by audio id ", song.Info.AudioId, " err ", err.Error())
-		}
-		bestMatches, err := db.GetBestMatchResultByAudioId(song.Info.AudioId, 1)
-		if err != nil {
-			println("err when get best result by audio id ", song.Info.AudioId, " err ", err.Error())
-		}
-		if len(bestMatches) > 0 && bestMatches[0].Id > 0 {
-			user, err := db.GetUserById(bestMatches[0].UserId)
-			if err != nil {
-				println("err when get user info by id ", bestMatches[0].UserId, " err ", err.Error())
-			}
-			song.Info.BestResult = model.ScoreResponse{
-				Youtube: MapYoutubeVideos[bestMatches[0].Id],
-				Match:   &bestMatches[0],
-				User: &model.BasicUserInfo{
-					Id:     user.Id,
-					Name:   user.Name,
-					Avatar: user.Avatar,
-				},
-			}
-		}
+		song := ListSong[i]
 		allSongs = append(allSongs, song.Info)
 	}
 	data, _ := json.Marshal(model.HttpResponse{Code: 0, Message: "", Data: allSongs})
